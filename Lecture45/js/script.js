50eee22ba9acd1527e911ae75ec68565addc1dce
Roy Fa-Lin Wang <falinw@umich.edu>
2019-03-25 12:11:39 -0400

    feat(Lecture45): Finish Lec 45

--- Lecture45/js/script.js
+++ Lecture45/js/script.js
@@ -1,29 +1,44 @@
 // Object creation
 var company = new Object();
 company.name = "Facebook";
+console.log(company);
+
 company.ceo = new Object();
 company.ceo.firstName = "Mark";
 company.ceo.favColor = "blue";
-
 console.log(company);
-console.log("Company CEO name is: "
-  + company.ceo.firstName);
+// >>> Uncaught TypeError: Cannot set property 'firstName' of undefined
+// var a;
+// a.hello = "hello"; // >>> Uncaught TypeError: Cannot set property 'hello' of undefined
+
+console.log("Company CEO name is: " + company.ceo.firstName);
+// >>> Company CEO name is: Mark
 
 console.log(company["name"]);
-var stockPropName = "stock of company";
-company[stockPropName] = 110;
+// >>> Facebook
 
-console.log("Stock price is: " +
-  company[stockPropName]);
+company.$stock = 110;
+console.log(company);
 
+company["stock of company"] = 120;
+console.log(company);
+console.log("Stock price is: " + company["stock of company"]);
+
+var stockPropName = "stock of Facebook";
+company[stockPropName] = 130;
+console.log("Stock price is: " + company[stockPropName]);
+
+// ------------Part 2-----------
 // Better way: object literal
-var facebook = {
-  name: "Facebook",
+var instagram = {
+  name: "Instagram",
   ceo: {
-    firstName: "Mark",
-    favColor: "blue"
+    firstName: "Adam",
+    favColor: "Purple",
   },
+  // $stock: 110,
   "stock of company": 110
 };
 
-console.log(facebook.ceo.firstName);
+console.log(instagram);
+console.log(instagram.ceo.firstName);
